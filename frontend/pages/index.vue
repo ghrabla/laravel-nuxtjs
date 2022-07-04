7102e6a6c55322344eab053bba4326575dc8add0
kamal <kamal@gmail.com>
2022-07-04 15:26:45 +0100

    err solved

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,10 +1,16 @@
 <template>
   <!-- <NuxtLogo/> -->
-  <Tutorial/>
+  <div>
+   <Navbar></Navbar>
+  <Tutorial></Tutorial>
+  <Footer></Footer>
+  </div>
 </template>
 
 <script>
+import Navbar from '../components/Navbar.vue'
 export default {
+  components: { Navbar },
   name: 'IndexPage'
 }
 </script>
