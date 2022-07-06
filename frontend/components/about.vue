ce71b106643288921a2f2ff0f56d2956ed6cd9e5
kamal <kamal@gmail.com>
2022-07-06 11:44:14 +0100

    iframe

--- frontend/components/about.vue
+++ frontend/components/about.vue
@@ -10,7 +10,7 @@ style="background: linear-gradient(90deg, #2b4554 0%, #767ba2 100%)"
   <div class="py-20 bg-cover bg-no-repeat bg-fixed" id="backimg">
     <div class="container m-auto text-center px-6 opacity-100">
       <h2 class="text-4xl font-bold mb-2 text-white">Echo Base...I've got something!</h2>
-      <h3 class="text-2xl mb-8 text-white-700 font-bold">Not much, but it could be a life form. This is Rouge Two. this is Rouge Two. Captain Solo, so you copy?</h3>
+      <h3 class="text-2xl mb-8 text-white font-bold">Not much, but it could be a life form. This is Rouge Two. this is Rouge Two. Captain Solo, so you copy?</h3>
       <button class="bg-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider hover:border-transparent hover:text-blue-500 hover:bg-gray-800">Commander Skywalker, do you copy?</button>
     </div>
   </div>
--- frontend/components/contact.vue
+++ frontend/components/contact.vue
@@ -1,12 +1,24 @@
 <template>
-    <div class="bg-gray-100 ">
+
+
+
+   <div>
+      <div class="bg-gray-100 container px-5 py-20 mx-auto ">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 text-center">Contact Us</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-center">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+       <div class="lg:flex">
+            <section>
+        <div class="responsive-map container px-1 py-10 mx-auto ">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2822.7806761080233!2d-93.29138368446431!3d44.96844997909819!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x52b32b6ee2c87c91%3A0xc20dff2748d2bd92!2sWalker+Art+Center!5e0!3m2!1sen!2sus!4v1514524647889" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+       </div>
+      </section>
+
        <section class="text-gray-600 body-font relative">
-  <div class="container px-5 py-24 mx-auto">
+  <div class="container px-1 py-10 mx-auto">
     <div class="flex flex-col text-center w-full mb-12">
-      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
-      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+      
     </div>
-    <div class="lg:w-1/2 md:w-2/3 mx-auto">
+    <div class="lg:w-5/6 md:w-2/3 mx-auto">
       <div class="flex flex-wrap -m-2">
         <div class="p-2 w-1/2">
           <div class="relative">
@@ -46,5 +58,7 @@
     </div>
   </div>
 </section> 
+       </div>
     </div>
+   </div>
 </template>
