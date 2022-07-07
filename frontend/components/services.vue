8d348db41cb1ad42f3c868b462c3bddf4bbb8ef6
kamal <kamal@gmail.com>
2022-07-07 17:01:41 +0100

    nuxt axios

--- frontend/components/services.vue
+++ frontend/components/services.vue
@@ -1,5 +1,13 @@
 <template>
-    <div class="bg-gray-100 ">
-        hi services
-    </div>
+  <h1>{{ post.title }}</h1>
 </template>
+
+<script>
+import axios from 'axios'
+export default {
+    async asyncData ({ $axios, params }) {
+      const  post  = await $axios.$get(`https://api.nuxtjs.dev/posts/${params.id}`)
+      return { post }
+    }
+}
+</script>
