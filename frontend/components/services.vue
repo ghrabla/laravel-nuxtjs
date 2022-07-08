5c072efa9d7ffa27e31277d80bbe79ab0ed1109e
kamal <kamal@gmail.com>
2022-07-08 11:36:17 +0100

    fetch err handling

--- frontend/components/services.vue
+++ frontend/components/services.vue
@@ -1,13 +1,25 @@
 <template>
-  <h1>{{ post.title }}</h1>
+  <div>
+    <p v-if="$fetchState.pending">Loading....</p>
+    <p v-else-if="$fetchState.error">Error while fetching mountains</p>
+    <ul v-else>
+      <li v-for="(mountain, index) in mountains" :key="index">
+        {{ mountain.title }}
+      </li>
+    </ul>
+  </div>
 </template>
-
 <script>
-import axios from 'axios'
-export default {
-    async asyncData ({ $axios, params }) {
-      const  post  = await $axios.$get(`https://api.nuxtjs.dev/posts/${params.id}`)
-      return { post }
+  export default {
+    data() {
+      return {
+        mountains: []
+      }
+    },
+    async fetch() {
+      this.mountains = await fetch(
+        'https://api.nuxtjs.dev/mountains'
+      ).then(res => res.json())
     }
-}
+  }
 </script>
