222b2fd04b5782b5b71fbf1e1747653be13348cb
kamal <kamal@gmail.com>
2022-07-18 12:25:29 +0100

    res finished

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -4,29 +4,37 @@
               Our services     
            </h1> 
 
-          <div class=" flex justify-evenly mt-20 lg:flex-row flex-col">
+          <div class=" flex justify-evenly mt-0 lg:mt-20 lg:flex-row flex-col">
             <div class="text-center">
-             <lottie-player src="https://assets9.lottiefiles.com/private_files/lf30_uxfkse56.json"  background="transparent"  speed="1"  style="width: 300px; height: 200px;"  loop autoplay></lottie-player>
+                <div class="lg:ml-0 ml-20 mt-10 lg:mt-0">
+                  <!-- <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_sl6vuo6t.json"  background="transparent"  speed="1"  style="width: 300px; height: 200px;"  loop autoplay></lottie-player> -->
+                  <lottie-player src="https://assets9.lottiefiles.com/private_files/lf30_uxfkse56.json"  background="transparent"  speed="1"  style="width: 300px; height: 200px;"  loop autoplay></lottie-player>
+                </div>
              <h2 class="font-bold text-xl text-center">make solars</h2>
              <p class="text-center">we work on s service from a long time  </p>
            </div>
             <div class="text-center">
-             <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_sl6vuo6t.json"  background="transparent"  speed="1"  style="width: 300px; height: 200px;"  loop autoplay></lottie-player>
+                <div class="lg:ml-0 ml-20 mt-10 lg:mt-0">
+                  <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_sl6vuo6t.json"  background="transparent"  speed="1"  style="width: 300px; height: 200px;"  loop autoplay></lottie-player>
+                </div>
              <h2 class="font-bold text-xl text-center">make solars</h2>
              <p class="text-center">we work on s service from a long time   </p>
            </div>
             <div class="text-center">
-             <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_ndkusx2f.json"  background="transparent"  speed="1"  style="width: 300px; height: 200px;"  loop autoplay></lottie-player>
+                <div class="lg:ml-0 ml-20 mt-10 lg:mt-0">
+                  <!-- <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_sl6vuo6t.json"  background="transparent"  speed="1"  style="width: 300px; height: 200px;"  loop autoplay></lottie-player> -->
+                <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_ndkusx2f.json"  background="transparent"  speed="1"  style="width: 300px; height: 200px;"  loop autoplay></lottie-player>
+                </div>
              <h2 class="font-bold text-xl text-center">make solars</h2>
              <p class="text-center">we work on s service from a long time  </p>
            </div>
           </div>
           
-          <div class="flex mt-20 ">
+          <div class="flex mt-20 flex-col lg:flex-row">
               <div class="w-full">
                   <img src="../assets/staticimg/3d-house-with-solar-pannels (1).jpg" alt="">
               </div>
-              <div class="bg-blue-400 w-4/5 ">
+              <div class="bg-blue-400 lg:w-4/5 w-full ">
                   <div class="mt-20">
                       <h1 class=" text-2xl font-bold text-center">FCES MAROC FOR YOUR BUSINESS</h1>
                       <p class="text-center">let's make the world more clean by using this good things</p>
