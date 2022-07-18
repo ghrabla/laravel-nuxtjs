37df67beb286ed15f9c5aab38e4a7c7c1846fdf2
kamal <kamal@gmail.com>
2022-07-18 12:04:36 +0100

    responsive it

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -13,13 +13,28 @@
             <div class="text-center">
              <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_sl6vuo6t.json"  background="transparent"  speed="1"  style="width: 300px; height: 200px;"  loop autoplay></lottie-player>
              <h2 class="font-bold text-xl text-center">make solars</h2>
-             <p class="text-center">we work on s service from a long time  </p>
+             <p class="text-center">we work on s service from a long time   </p>
            </div>
             <div class="text-center">
              <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_ndkusx2f.json"  background="transparent"  speed="1"  style="width: 300px; height: 200px;"  loop autoplay></lottie-player>
              <h2 class="font-bold text-xl text-center">make solars</h2>
              <p class="text-center">we work on s service from a long time  </p>
            </div>
+          </div>
+          
+          <div class="flex mt-20 ">
+              <div class="w-full">
+                  <img src="../assets/staticimg/3d-house-with-solar-pannels (1).jpg" alt="">
+              </div>
+              <div class="bg-blue-400 w-4/5 ">
+                  <div class="mt-20">
+                      <h1 class=" text-2xl font-bold text-center">FCES MAROC FOR YOUR BUSINESS</h1>
+                      <p class="text-center">let's make the world more clean by using this good things</p>
+                   <button class="ml-40 mt-5 bg-transparent hover:bg-transparent hover:border-transparent text-white font-semibold hover:text-white  hover:bg-blue-700 py-2 px-4 border  border-blue-700 rounded">
+                     Get In Touch <i class="fa-solid fa-arrow-right"></i>
+                     </button>
+                  </div>
+              </div>
           </div>
         </div>
         
