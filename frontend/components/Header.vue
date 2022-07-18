16c3691aca312faa230b7ad14942dcd4dfeaf4ab
kamal <kamal@gmail.com>
2022-07-18 10:43:34 +0100

    doing good

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -1,6 +1,26 @@
 <template>
-        <h1 class="text-center font-bold text-3xl">  
-          Our services     
-        </h1> 
+        <div>
+            <h1 class="text-center font-bold text-3xl mt-10">  
+              Our services     
+           </h1> 
+
+          <div class="flex justify-evenly mt-20">
+            <div class="">
+             <img src="" alt="">
+             <h2 class="font-bold text-xl text-center">make solars</h2>
+             <p class="text-center">we work on s service from a long time  </p>
+           </div>
+            <div class="">
+             <img src="" alt="">
+             <h2 class="font-bold text-xl text-center">make solars</h2>
+             <p class="text-center">we work on s service from a long time  </p>
+           </div>
+            <div class="">
+             <img src="" alt="">
+             <h2 class="font-bold text-xl text-center">make solars</h2>
+             <p class="text-center">we work on s service from a long time  </p>
+           </div>
+          </div>
+        </div>
         
 </template>
