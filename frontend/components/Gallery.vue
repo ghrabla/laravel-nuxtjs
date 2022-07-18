d9197a574993204a105a29c5643a9402d133c5e7
kamal <kamal@gmail.com>
2022-07-18 15:03:40 +0100

    home still

--- frontend/components/Gallery.vue
+++ frontend/components/Gallery.vue
@@ -1,6 +1,51 @@
 <template>
     <div>
-        hi
+            <div class="container mx-auto px-4"> 
+                
+      <section class="py-8 px-4">
+        <div class="flex flex-wrap -mx-4">
+          <div class="hidden md:block md:w-1/2 px-4">
+            <div class="h-full w-full bg-cover rounded shadow-md" style="background-image: url('https://source.unsplash.com/random/1280x720')"></div>
+          </div>
+          <div class="md:w-1/2 h-auto px-4">
+            <div class="mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""></div>
+            <div><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""></div>
+          </div>
+        </div>
+      </section>
+                
+      <section class="py-8 px-4">
+        <div class="flex flex-wrap -mx-4">
+          <div class="md:w-1/2 px-4 mb-8 md:mb-0"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""></div>
+          <div class="md:w-1/2 px-4 mb-8 md:mb-0"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""></div>
+        </div>
+      </section>
+                
+      <section class="pt-8 px-4">
+        <div class="flex flex-wrap -mx-4">
+          <div class="md:w-1/3 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""></div>
+          <div class="md:w-1/3 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""></div>
+          <div class="md:w-1/3 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""></div>
+          <div class="md:w-1/3 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""></div>
+          <div class="md:w-1/3 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""></div>
+          <div class="md:w-1/3 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""></div>
+        </div>
+      </section>
+                
+      <section class="py-8 px-4">
+        <div class="flex flex-wrap -mx-4 -mb-8">
+          <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""></div>
+          <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""></div>
+          <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""></div>
+          <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""></div>
+          <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""></div>
+          <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""></div>
+          <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""></div>
+          <div class="md:w-1/4 px-4 mb-8"><img class="rounded shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""></div>
+        </div>
+      </section>
+    </div>
+
     </div>
 </template>
 
