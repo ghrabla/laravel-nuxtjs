260d514df6325a35170b3b85092a0b3c409012c4
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-07-27 17:40:46 +0100

    fixing err

--- frontend/components/events.vue
+++ frontend/components/events.vue
@@ -1,2 +1,36 @@
-
+<template>
+  <div>
+    <p v-if="$fetchState.pending">Loading....</p>
+    <p v-else-if="$fetchState.error">Error while fetching mountains</p>
+    <ul v-else>
+      <div>{{mountains}}</div>
+      <li v-for="(mountain, index) in mountains" :key="index">
+        {{ mountain.title }}
+      </li>
+      <button @click="refresh">Refresh</button>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        mountains: []
+      }
+    },
+    async fetch() {
+      this.mountains = await fetch(
+        'https://api.nuxtjs.dev/mountains'
+      ).then(res => res.json())
+    },
+     methods: {
+      refresh() {
+        this.$nuxt.refresh()
+      }
+    }
+  }
+</script>
+<style scoped>
+ 
+</style>
 
