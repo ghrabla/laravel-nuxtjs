7f619eeb84a4803cfd4ea649e9d20e1aaeb00165
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-07-29 12:21:01 +0100

    vuex without com

--- frontend/components/events.vue
+++ frontend/components/events.vue
@@ -1,36 +1,70 @@
 <template>
   <div>
-    <p v-if="$fetchState.pending">Loading....</p>
-    <p v-else-if="$fetchState.error">Error while fetching mountains</p>
-    <ul v-else>
-      <div>{{mountains}}</div>
-      <li v-for="(mountain, index) in mountains" :key="index">
-        {{ mountain.title }}
+    <input type="text" @keyup.enter="addTodo" placeholder="What needs to be done?">
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <input :checked="todo.done" @change="toggle(todo)" type="checkbox" :id="todo.id">
+        <label :class="{ done: todo.done }" :for="todo.id">{{ todo.text }}</label>
+        <button @click="removeTodo(todo)">remove</button>
       </li>
-      <button @click="refresh">Refresh</button>
     </ul>
   </div>
 </template>
+
 <script>
-  export default {
-    data() {
-      return {
-        mountains: []
-      }
+import { mapMutations } from 'vuex'
+
+export default {
+  computed: {
+    todos () {
+      return this.$store.state.todos.list
+    }
+  },
+  methods: {
+    addTodo (event) {
+      this.$store.commit('todos/add', event.target.value)
+      event.target.value = ''
     },
-    async fetch() {
-      this.mountains = await fetch(
-        'https://api.nuxtjs.dev/mountains'
-      ).then(res => res.json())
-    },
-     methods: {
-      refresh() {
-        this.$nuxt.refresh()
-      }
+    ...mapMutations({
+      toggle: 'todos/toggle'
+    }),
+    removeTodo (todo){
+      this.$store.commit('todos/remove', todo)
     }
   }
+}
 </script>
-<style scoped>
- 
-</style>
 
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+ul {
+  display: inline-flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5rem;
+}
+
+input[type="checkbox"] {
+  margin: 0.5rem;
+}
+
+button {
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+}
+
+.done {
+  text-decoration: line-through;
+}
+</style>
