282464c206d68a214aee65826cf5a5293de14e04
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-07-29 15:05:39 +0100

    cart flex

---
+++ frontend/pages/cart.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <navbar></navbar>
+      <panier></panier>
+      <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Footer from '../components/Footer.vue'
+import Panier from '../components/panier.vue'
+export default {
+  components: { Footer, Panier },
+
+}
+</script>
+
+<style>
+
+</style>
