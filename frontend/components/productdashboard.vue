720d264cf5de296c8f7041c7ce37d4c73f7dd7fa
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-08-07 02:21:46 +0100

    dashboard update

--- frontend/components/productdashboard.vue
+++ frontend/components/productdashboard.vue
@@ -4,6 +4,67 @@
 <!-- <div v-if="show" @click="show=!show" class="bg-gray-700 w-full h-full absolute"></div> -->
 <addform v-if="show"></addform>
 
+<form  class="w-full max-w-lg absolute bg-gray-500 rounded  p-10 mx-auto ml:0 lg:ml-80 shadow-md z-50	absolute" v-if="showupdate">
+   <a href="#" @click="showupdate=!showupdate" class="text-white font-bold flex justify-end text-xl mb-5"><i class="fa-solid fa-xmark"></i></a>
+  <div class="text-center text-white font-bold mb-4">Update Product</div>
+  <div class="flex flex-wrap -mx-3 mb-6 ">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2" for="grid-first-name">
+        Product Name
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe">
+      <!-- <p class="text-red-500 text-xs italic">Please fill out this field.</p> -->
+    </div>
+    <div class="w-full md:w-1/2 px-3">
+      <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2" for="grid-last-name">
+        Product Price
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe">
+    </div>
+  </div>
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full px-3">
+      <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2" for="grid-password">
+        Description
+      </label>
+      <textarea class="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password"  ></textarea>
+      <p class="text-white text-xs italic">Make it as long and as crazy as you'd like</p>
+    </div>
+  </div>
+   <div class="flex flex-wrap -mx-3 mb-6">
+  <div class="w-full px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2" for="grid-city">
+        picture
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="file" >
+    </div>
+  </div>
+  <div class="flex -mx-3 mb-2">
+    
+    <div class="w-full px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2" for="grid-state">
+        Type
+      </label>
+      <div class="relative">
+        <select class="block appearance-none w-full bg-gray-200 bordeblackder-gray-200 text-black py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+          <option>variator</option>
+          <option>solar</option>
+          <option>battery</option>
+        </select>
+        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-white">
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+        </div>
+      </div>
+    </div>
+    <div class="w-full px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2" for="grid-zip">
+        Quantity
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210">
+    </div>
+  </div>
+</form>
+
   <div class="container mx-auto px-4 sm:px-8">
   <div class="py-8">
     <div>
@@ -102,7 +163,7 @@
                 <button class="font-bold text-xl" v-if="showaction==n" @click="showaction=!showaction"><i class="fa-solid fa-xmark"></i></button>
               </td>
               <div class="flex flex-col gap-3" v-if="showaction==n">
-                  <button class="text-green-500 font-bold"><i class="fas fa-edit"></i> Update {{n}}</button>
+                  <button class="text-green-500 font-bold" @click="showupdate=!showupdate"><i class="fas fa-edit"></i> Update {{n}}</button>
                   <button class="text-red-500 font-bold"><i class="fa fa-trash" aria-hidden="true"></i> Delete</button>
               </div> 
             </tr>
@@ -112,6 +173,8 @@
     </div>
   </div>
 </div>
+
+
 
 </div>
 		
@@ -125,6 +188,7 @@
    data(){
      return{
        showaction: false,
+       showupdate: false,
        ns: 5,
        n : ''
      }
